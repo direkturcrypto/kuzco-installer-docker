7ae71f559b896bfc337bcc8d4578ad06138e398f
direkturcrypto <168287805+direkturcrypto@users.noreply.github.com>
2025-06-09 12:00:50 +0700

    Update start.sh

--- kuzco-main/start.sh
+++ kuzco-main/start.sh
@@ -3,7 +3,7 @@
 # Periksa argumen yang diberikan
 if [ "$1" == "serve" ]; then
     # Jalankan perintah node
-    cd /app && NODE_PORT=14444 ./vikey-inference-linux
+    cd /app && NODE_PORT=14444 DEFAULT_MODEL=llama3.2-3b-instruct ./vikey-inference-linux
 else
     exit 1
 fi
