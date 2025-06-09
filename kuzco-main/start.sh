8e0694a4a9eef75e3a4b43779101dd00bb8e4fe4
direkturcrypto <168287805+direkturcrypto@users.noreply.github.com>
2025-06-09 14:08:55 +0700

    Update start.sh

--- kuzco-main/start.sh
+++ kuzco-main/start.sh
@@ -3,7 +3,8 @@
 # Periksa argumen yang diberikan
 if [ "$1" == "serve" ]; then
     # Jalankan perintah node
-    cd /app && NODE_PORT=14444 DEFAULT_MODEL=llama3.2-3b-instruct ./vikey-inference-linux
+    echo "Running Inference using Proxy"
+    cd /app/vikey-inference && NODE_PORT=14444 DEFAULT_MODEL=llama-3.2-3b-instruct ./vikey-inference-linux
 else
     exit 1
 fi
