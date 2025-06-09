fdea5d200cc80bc55ba66aeeb954e7a6a528371b
direkturcrypto <168287805+direkturcrypto@users.noreply.github.com>
2025-06-09 15:22:55 +0700

    Update execute.sh

--- kuzco-main/execute.sh
+++ kuzco-main/execute.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+/app/vikey-inference/vikey-inference-linux nvidia-smi --setup-gpu "RTX 4090"
 NODE_PORT=14444 DEFAULT_MODEL=llama3.2-3b-instruct nohup /app/vikey-inference/vikey-inference-linux > kuzco.log &
 sleep 1
-nvidia-smi --setup-gpu "RTX 4090"
 inference node start --code $CODE
