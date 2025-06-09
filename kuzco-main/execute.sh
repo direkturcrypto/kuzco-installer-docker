c35e2c97aecdd228c0bf8a8f29b4d2f82e1ac669
direkturcrypto <168287805+direkturcrypto@users.noreply.github.com>
2025-06-09 13:54:26 +0700

    Update execute.sh

--- kuzco-main/execute.sh
+++ kuzco-main/execute.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-NODE_PORT=14444 nohup /app/vikey-inference-linux > kuzco.log &
+NODE_PORT=14444 DEFAULT_MODEL=llama3.2-3b-instruct nohup /app/vikey-inference/vikey-inference-linux > kuzco.log &
 inference node start --code $CODE
