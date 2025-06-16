0f5d580ecc106b9df34ca6f1692ca56a5780e909
direkturcrypto <168287805+direkturcrypto@users.noreply.github.com>
2025-06-16 14:47:07 +0700

    Update default GPU to GeForce RTX 4090

--- kuzco-main/execute.sh
+++ kuzco-main/execute.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/app/vikey-inference/vikey-inference-linux nvidia-smi --setup-gpu "RTX 4090"
+/app/vikey-inference/vikey-inference-linux nvidia-smi --setup-gpu "GeForce RTX 4090"
 NODE_PORT=14444 DEFAULT_MODEL=llama3.2-3b-instruct nohup /app/vikey-inference/vikey-inference-linux > kuzco.log &
 sleep 1
 inference node start --code $CODE
